b61426c97b6cbc657097c7c2f4fba817525d032b
Eduardo <eduardo.campesato.7@gmail.com>
2022-03-23 10:19:53 -0300

    Exercício Ebac UI

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -26,8 +26,8 @@
 
 
 Cypress.Commands.add('login', (usuario, senha) => {
-    cy.get('#username').type(usuario)
-    cy.get('#password').type(senha, {log: false})
+    cy.get('#username').type('aluno_ebac@teste.com')
+    cy.get('#password').type('teste@teste.com', {log: false})
     cy.get('.woocommerce-form > .button').click()
 });
 
